edfc2d5be02ce71276acc6513c037f48a92d49b9
Haziel Revert <hazielrevert@gmail.com>
2022-11-24 19:38:47 -0300

    primer desafio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './partials/header/header.component';
+import { SidebarComponent } from './partials/sidebar/sidebar.component';
+import { TarjetaComponent } from './partials/tarjeta/tarjeta.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    TarjetaComponent,
+    
   ],
   imports: [
     BrowserModule,
