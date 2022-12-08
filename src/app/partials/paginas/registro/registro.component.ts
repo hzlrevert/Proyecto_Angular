3f307fd2f4f2f1c0a9846d550a7cdb3c5b6c9c9a
Haziel Revert <hazielrevert@gmail.com>
2022-12-08 17:59:53 -0300

    formulario completo

--- src/app/partials/paginas/registro/registro.component.ts
+++ src/app/partials/paginas/registro/registro.component.ts
@@ -14,11 +14,13 @@ export class RegistroComponent {
 firstNameControl = new FormControl('Nombre', [Validators.required])
 lastNameControl = new FormControl('Apellido', [Validators.required])
 passControl = new FormControl('Contraseña', [Validators.required, Validators.minLength(8), Validators.maxLength(10)  ] )
+emailControl = new FormControl('Email', [Validators.required, Validators.email] )
 
 
 registerForm = new FormGroup({
   firstName: this.firstNameControl,
   lastName: this.lastNameControl,
   pass: this.passControl,
+  email: this.emailControl,
 })
 }
